6fe07a62d687cd3adf535cc4c54ac25e228adcdc
Gabby Nunez <gabbynunez@Gabbys-MacBook-Pro.local>
2022-12-02 11:49:48 -0600

    DOM layout was created.

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -4,6 +4,17 @@ const URL = 'http://localhost:9000/api/todos'
 
 export default class App extends React.Component {
   render() {
-    return null
+    return (
+      <div>
+        <h1>Todos Checklist</h1>
+        
+          <li>1</li>
+          <li>2</li>
+        
+        <input placeholder='todo goes here' />
+        <button>Add</button>
+        <button>Clear Completed Todo</button>
+      </div>
+    )
   }
 }
