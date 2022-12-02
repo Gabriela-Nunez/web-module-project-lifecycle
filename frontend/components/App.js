ba3003866c42b9a236e78ae86db19c5177ebed4a
Gabby Nunez <gabbynunez@Gabbys-MacBook-Pro.local>
2022-12-02 12:18:16 -0600

    Connected DOM to state.

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,19 +1,43 @@
 import React from 'react'
+import axios from 'axios';
+import { ThemeProvider } from 'styled-components';
 
 const URL = 'http://localhost:9000/api/todos'
 
 export default class App extends React.Component {
+
+  state = {
+    todos: [],
+  }
+
+  getTodos = () => {
+    axios.get(URL)
+    .then(res => {
+      this.setState({ ...this.state, todos: res.data.data})
+    })
+    .catch(err => {
+      console.error(err);
+    })
+  }
+
+  componentDidMount() {
+    this.getTodos()
+  }
+
   render() {
     return (
       <div>
-        <h1>Todos Checklist</h1>
+        <h1>Todos Checklist:</h1>
         
-          <li>1</li>
-          <li>2</li>
+          {
+            this.state.todos.map(todo => {
+              return <li key={todo.id}>{todo.name}</li>
+            })
+          }
         
-        <input placeholder='todo goes here' />
-        <button>Add</button>
-        <button>Clear Completed Todo</button>
+        <input type='text' placeholder='Type Todo Here' />
+        <input type='submit' />
+        <button>Clear Completed</button>
       </div>
     )
   }
